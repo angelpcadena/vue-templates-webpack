f5991ebef02f99464403710eddce3be49acbb997
angelpcadena <angelpcadena@gmail.com>
2020-02-21 00:00:57 -0600

    added vuex

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
 import vuetify from './plugins/vuetify'
 import router from './plugins/router'
+
 import App from './App'
 
 import /* webpackPrefetch: true */ "./styles/nocnok-icons.css"
 import /* webpackPrefetch: true */ "./styles/montserrat.css"
 import /* webpackPrefetch: true */ "./styles/source-sans-pro.css"
+
+Vue.use(Vuex)
 
 new Vue({
 	vuetify,
